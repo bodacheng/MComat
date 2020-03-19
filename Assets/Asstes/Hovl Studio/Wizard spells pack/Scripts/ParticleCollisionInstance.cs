03d0c80a324fb3380beee6a330a756ca17432fcc
伯大成 <bodacheng123@gmail.com>
2020-03-19 22:44:05 +0900

    AAA projections 1

--- Assets/Asstes/Hovl Studio/Wizard spells pack/Scripts/ParticleCollisionInstance.cs
+++ Assets/Asstes/Hovl Studio/Wizard spells pack/Scripts/ParticleCollisionInstance.cs
@@ -6,13 +6,10 @@ using System.Collections.Generic;
 public class ParticleCollisionInstance : MonoBehaviour
 {
     public GameObject[] EffectsOnCollision;
+    public float Offset = 0;
     public float DestroyTimeDelay = 5;
     public bool UseWorldSpacePosition;
-    public float Offset = 0;
-    public Vector3 rotationOffset = new Vector3(0,0,0);
-    public bool useOnlyRotationOffset = true;
     public bool UseFirePointRotation;
-    public bool DestoyMainEffect = true;
     private ParticleSystem part;
     private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
     private ParticleSystem ps;
@@ -29,20 +26,12 @@ public class ParticleCollisionInstance : MonoBehaviour
             foreach (var effect in EffectsOnCollision)
             {
                 var instance = Instantiate(effect, collisionEvents[i].intersection + collisionEvents[i].normal * Offset, new Quaternion()) as GameObject;
-                if (!UseWorldSpacePosition) instance.transform.parent = transform;
                 if (UseFirePointRotation) { instance.transform.LookAt(transform.position); }
-                else if (rotationOffset != Vector3.zero && useOnlyRotationOffset) { instance.transform.rotation = Quaternion.Euler(rotationOffset); }
-                else
-                {
-                    instance.transform.LookAt(collisionEvents[i].intersection + collisionEvents[i].normal);
-                    instance.transform.rotation *= Quaternion.Euler(rotationOffset);
-                }
+                else { instance.transform.LookAt(collisionEvents[i].intersection + collisionEvents[i].normal); }
+                if (!UseWorldSpacePosition) instance.transform.parent = transform;
                 Destroy(instance, DestroyTimeDelay);
             }
         }
-        if (DestoyMainEffect == true)
-        {
-            Destroy(gameObject, DestroyTimeDelay + 0.5f);
-        }
+        Destroy(gameObject, DestroyTimeDelay + 0.5f);
     }
 }
