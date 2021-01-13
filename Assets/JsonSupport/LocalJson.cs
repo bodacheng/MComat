02f6c3dc9a33019a6fe903bd596110dd5f58fdc3
伯大成 <bodacheng123@gmail.com>
2021-01-13 11:08:49 +0900

    s

---
+++ Assets/JsonSupport/LocalJson.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using UnityEngine;
+using System;
+
+namespace Json
+{
+    public static class LocalJson
+    {
+        public static void SaveInfoToJsonFile_persistentDataPath(string subpath, string filename, string json)
+        {
+            //string wholepath = Path.Combine(Application.persistentDataPath, subpath);
+            string wholepath;
+            if (subpath != null)
+            {
+                if (!Directory.Exists(Application.persistentDataPath + "/" + subpath))
+                {
+                    //if it doesn't, create it
+                    Directory.CreateDirectory(Application.persistentDataPath + "/" + subpath);
+                }
+                wholepath = Application.persistentDataPath + "/" + subpath + "/" + filename;
+            }
+            else
+            {
+                wholepath = Application.persistentDataPath + "/" + filename;
+            }
+
+            try
+            {
+                if (!File.Exists(wholepath))
+                {
+                    File.Create(wholepath).Close();
+                }
+                File.WriteAllText(wholepath, json, System.Text.Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+        
+        public static void SaveInfoToJsonFile_dataPath(string subpath, string filename, string json)
+        {
+            //string wholepath = Path.Combine(Application.persistentDataPath, subpath);
+            string wholepath;
+            if (subpath != null)
+            {
+                if (!Directory.Exists(Application.dataPath + "/" + subpath))
+                {
+                    //if it doesn't, create it
+                    Directory.CreateDirectory(Application.dataPath + "/" + subpath);
+                }
+                wholepath = Application.dataPath + "/" + subpath + "/" + filename;
+            }
+            else
+            {
+                wholepath = Application.dataPath + "/" + filename;
+            }
+            
+            try
+            {
+                if (!File.Exists(wholepath))
+                {
+                    File.Create(wholepath).Close();
+                }
+                Debug.Log("文件生成"+ wholepath);
+                File.WriteAllText(wholepath, json, System.Text.Encoding.UTF8);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+        
+        public static void DeleteAllUnderFolder(string filePath)
+        {
+            try
+            {
+                if (Directory.Exists(filePath))
+                {
+                    foreach (string file in Directory.GetFiles(filePath))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.ToString());
+            }
+        }
+    }
+}
+
