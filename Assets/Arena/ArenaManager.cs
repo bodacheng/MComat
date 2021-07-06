f31fc9e91e18cfe442b233e2f5646b31decda2f3
伯大成 <bodacheng123@gmail.com>
2021-07-06 15:45:54 +0900

    dwef

--- Assets/Arena/ArenaManager.cs
+++ Assets/Arena/ArenaManager.cs
@@ -1,12 +1,5 @@
-﻿using System.Collections;
-using Api.Dto.Model;
-using UnityEngine;
-using PlayFab;
-using PlayFab.ClientModels;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using Newtonsoft.Json;
-using System;
-using mainMenu;
 using dataAccess;
 
 public class ArenaManager : MonoBehaviour
@@ -15,8 +8,10 @@ public class ArenaManager : MonoBehaviour
     
     public RectTransform ArenaCanvas;
     public ArenaFightTeamDisplay myTeam; // 玩家队伍显示
-    public ArenaFightTeamDisplay Fight1, Fight2, Fight3, Fight4; // 挑战玩家队伍显示
     
+    public RectTransform EnemiesT;
+    public ArenaFightTeamDisplay ArenaFightTeamDisplayPrefab;
+
     void Awake()
     {
         target = this;
@@ -28,7 +23,15 @@ public class ArenaManager : MonoBehaviour
         CloudScript.GetLeaderboardAroundUser(
             (List<LeaderboardInfo> obj) =>
             {
-                LoadArena(obj);
+                List<LeaderboardInfo> exceptSelf = new List<LeaderboardInfo>();
+                for (int i = 0; i < obj.Count; i++)
+                {
+                    if (obj[i].PlayerLeaderboardEntry.PlayFabId != Account._AccInfo.playerID)
+                    {
+                        exceptSelf.Add(obj[i]);
+                    }
+                }
+                LoadArena(exceptSelf);
                 ArenaCanvas.gameObject.SetActive(true);
             } ,
             () =>
@@ -38,53 +41,21 @@ public class ArenaManager : MonoBehaviour
         );
     }
     
-    public static void SaveDefend(MultiDict<int, int, CharDataInfo> myteam, Action<int> finished)
-    {
-        switch (Account.ReferenceMode)
-        {
-            case PlayerInfoRefMode.localTestSaveData:
-                break;
-            case PlayerInfoRefMode.remoteTestPlayer:
-                PlayFabClientAPI.UpdateUserData(
-                    new UpdateUserDataRequest()
-                    {
-                        Data = new Dictionary<string, string>()
-                        {
-                            {"defendTeam", JsonConvert.SerializeObject(myteam._SerializableSets) }
-                        }
-                    },
-                    result => Debug.Log("Successfully Saved DefendTeam"),
-                    errorCallback => {
-                        Debug.Log(errorCallback.Error);
-                    }
-                );
-                break;
-            case PlayerInfoRefMode.formalVersion:
-                break;
-        }
-    }
-
     // 挑战玩家队伍机能加载（目前规定显示在画面上的挑战组一共四个。远程获取不到的情况下就本地生成）
     public void LoadArena(List<LeaderboardInfo> leaderboards)
     {
         myTeam.ShowMyTeam();
+        foreach (Transform c in EnemiesT)
+        {
+            Destroy(c.gameObject);
+        }
         for (int i = 0; i < leaderboards.Count; i++)
         {
-            switch (i)
-            {
-                case 0:
-                    Fight1.AddFightToList(leaderboards[i]);
-                    break;
-                case 1:
-                    Fight2.AddFightToList(leaderboards[i]);
-                    break;
-                case 2:
-                    Fight3.AddFightToList(leaderboards[i]);
-                    break;
-                case 3:
-                    Fight4.AddFightToList(leaderboards[i]);
-                    break;
-            }
+            ArenaFightTeamDisplay o = Instantiate(ArenaFightTeamDisplayPrefab);
+            o.AddFightToList(leaderboards[i]);
+            o.transform.SetParent(EnemiesT);
+            o.transform.localPosition = Vector3.zero;
+            o.transform.localScale = Vector3.one;
         }
     }
 }
