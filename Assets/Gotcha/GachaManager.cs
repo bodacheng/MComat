8fc08691bd814fb879979fdf5acea34b60d9c146
伯大成 <bodacheng123@gmail.com>
2021-07-06 22:44:08 +0900

    234v

--- Assets/Gotcha/GachaManager.cs
+++ Assets/Gotcha/GachaManager.cs
@@ -32,32 +32,10 @@ public class GachaManager : MonoBehaviour
     
     public void OneTime()
     {
-        IEnumerator Go()
-        {
-            IEnumerator process = target.Gacha("human", 1);
-            yield return process;
-            if ((bool)process.Current)
-                PreScene.target.trySwitchToStep(MainSceneStep.GotchaAnim, true);
-            else{
-                Debug.Log("错误返回" + process.Current);
-            }
-        }
-        PreScene.target.mainProcessRunner.RunAsQueued(Go());
     }
     
     public void NineTimes()
     {
-        IEnumerator Go()
-        {
-            IEnumerator process = target.Gacha("human", 9);
-            yield return process;
-            if (process.Current != null)
-                PreScene.target.trySwitchToStep(MainSceneStep.GotchaAnim, true);
-            else{
-                Debug.Log("错误返回" + process.Current);
-            }
-        }
-        PreScene.target.mainProcessRunner.RunAsQueued(Go());
     }
 
     public void GetAllSK()
@@ -85,58 +63,5 @@ public class GachaManager : MonoBehaviour
             Result = stones;
             PreScene.target.trySwitchToStep(MainSceneStep.GotchaAnim, true);
         } 
-    }
-
-    public IEnumerator Gacha(string type, int count)
-    {
-        List<StoneOfPlayerInfo> Results = null;
-        switch (Account.ReferenceMode)
-        {
-            case PlayerInfoRefMode.localTestSaveData:
-                IEnumerator GET = Gotcha(type, count);
-                yield return GET;
-                Results = (List<StoneOfPlayerInfo>)GET.Current;
-                break;
-            case PlayerInfoRefMode.remoteTestPlayer:
-
-                break;
-            case PlayerInfoRefMode.formalVersion:
-                break;
-        }
-        Result = Results;
-        yield return true;
-    }
-    
-    public static IEnumerator Gotcha(string type, int stoneCount)
-    {
-        List<StoneOfPlayerInfo> Geted = new List<StoneOfPlayerInfo>();
-        for (int i = 0; i < stoneCount; i++)
-        {
-            IEnumerator GET = OneGocha(type);
-            yield return GET;
-            Geted.Add((StoneOfPlayerInfo)GET.Current);
-        }
-        yield return Geted;
-    }
-    
-    // Gocha对象技能石应该有个更明确的范围，因为开启不同的gocha项目，出的东西还不一样，这应该是比较普遍的。更何况还有个不同获得概率的问题。
-    // 这个函数摆在这地方本身是违背原则的。
-    static IEnumerator OneGocha(string type)
-    {
-        List<SkillConfig> skillConfigs = SkillConfigTable.GetSkillConfigsOfType(type);
-        int random_index = Random.Range(0, skillConfigs.Count);
-        SkillConfig skillConfig = skillConfigs[random_index];
-        StoneOfPlayerInfo stoneInfo = new StoneOfPlayerInfo
-        {
-            InstanceId = Stones.GetNonRepeatID_LocalSave(),
-            skillId = skillConfig.RECORD_ID,
-            EXP = 0,
-            BreakThrough = 0,
-            Inherent = "false",
-            inUsingMonsterOfPlayerId = "-1",
-            inUsingSkillSlot = null
-        };
-        Stones.Add(stoneInfo);
-        yield return stoneInfo;
     }
 }
