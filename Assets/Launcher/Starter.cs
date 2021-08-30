8395131df366816d40b96538d9f851ddfbceede0
伯大成 <bodacheng123@gmail.com>
2021-08-30 21:46:50 +0900

    Login bonus. 初

--- Assets/Launcher/Starter.cs
+++ Assets/Launcher/Starter.cs
@@ -83,6 +83,7 @@ public class Starter : MonoBehaviour
                 {
                     playerID = result.PlayFabId
                 };
+                PlayFabLogin.CheckIn();
                 EnterFrontScene();
             },
             fail => {
--- Assets/Login/PlayFabLogin.cs
+++ Assets/Login/PlayFabLogin.cs
@@ -2,6 +2,7 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using System;
+using System.Collections.Generic;
 
 public class PlayFabLogin
 {
@@ -16,5 +17,70 @@ public class PlayFabLogin
             sucess,
             fail
         );
+    }
+    
+    public static void CheckIn()
+    {
+        Debug.Log("Checking-in with Server...");
+        ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest() { 
+            FunctionName = "CheckIn", 
+        };
+		
+        PlayFabClientAPI.ExecuteCloudScript(request, OnCheckInCallback, OnApiCallError);
+    }
+    
+    static void OnCheckInCallback(ExecuteCloudScriptResult result)
+    {
+        // output any errors that happend within cloud script
+        if(result.Error != null)
+        {
+            Debug.LogError(string.Format("{0} -- {1}", result.Error, result.Error.Message));
+            return;
+        }	
+
+        Debug.Log("CheckIn Results:");
+
+        var serializer = PluginManager.GetPlugin<ISerializerPlugin>(PluginContract.PlayFab_Serializer);
+        List<ItemInstance> grantedItems = serializer.DeserializeObject<List<ItemInstance>>(result.FunctionResult.ToString());
+		
+        if(grantedItems != null && grantedItems.Count > 0)
+        {
+            Debug.Log(string.Format("You were granted {0} items:", grantedItems.Count));
+			
+            string output = string.Empty;
+            foreach(var item in grantedItems)
+            {
+                output += string.Format("\t {0}: {1}\n", item.ItemId, item.Annotation);
+            }
+            Debug.Log(output);
+        }
+        else if(result.Logs.Count > 0)
+        {
+            foreach(var statement in result.Logs)
+            {
+                Debug.Log(statement.Message);
+            }
+        }
+        else
+        {
+            Debug.Log("CheckIn Successful! No items granted.");
+        }
+    }
+	
+    static void OnApiCallError(PlayFabError err)
+    {
+        string http = string.Format("HTTP:{0}", err.HttpCode);
+        string message = string.Format("ERROR:{0} -- {1}", err.Error, err.ErrorMessage);
+        string details = string.Empty;
+		
+        if(err.ErrorDetails != null)
+        {
+            foreach(var detail in err.ErrorDetails)
+            {
+                details += string.Format("{0} \n", detail.ToString());
+            }
+        }
+		
+        Debug.LogError(string.Format("{0}\n {1}\n {2}\n", http, message, details));
     }
 }
