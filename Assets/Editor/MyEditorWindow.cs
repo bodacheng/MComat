05ded10d9f13287d34c39cef2a6aada928df016e
伯大成 <bodacheng123@gmail.com>
2022-05-23 18:19:40 +0900

    temp

--- Assets/Editor/MyEditorWindow.cs
+++ Assets/Editor/MyEditorWindow.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor.AddressableAssets;
 using UnityEngine;
 
 #if UNITY_EDITOR
@@ -5,6 +7,11 @@ using UnityEditor;
 
 public static class MyEditorWindow
 {
+    readonly static Dictionary<int,string> menuPath = new Dictionary<int,string>(){
+        {0, "P3/AddressableAssets/PlayMode/Use Local"},
+        {2, "P3/AddressableAssets/PlayMode/Use Remote"}
+    };
+    
     [MenuItem("MCombat/StageManager", priority = 1)]
     static void StageManager()
     {
@@ -35,6 +42,26 @@ public static class MyEditorWindow
         OtherTool window = (OtherTool)EditorWindow.GetWindow(typeof(OtherTool));
         window.titleContent = new GUIContent("其他工具");
         window.Show();
+    }
+    
+    static MyEditorWindow()
+    {
+        EditorApplication.delayCall += () => ChangePlayMode(AddressableAssetSettingsDefaultObject.Settings.ActivePlayModeDataBuilderIndex);
+    }
+    
+    [MenuItem("MCombat/AddressableAssets/PlayMode/Use Local")]
+    static void PlayModeUseLocal() => ChangePlayMode(0);
+    [MenuItem("MCombat/AddressableAssets/PlayMode/Use Remote")]
+    static void PlayModeUseRemote() => ChangePlayMode(2);
+    
+    static void ChangePlayMode(int mode)
+    {
+        foreach ( var kv in menuPath)
+        {
+            Menu.SetChecked(kv.Value, mode==kv.Key);
+        }
+
+        AddressableAssetSettingsDefaultObject.Settings.ActivePlayModeDataBuilderIndex = mode;
     }
 }
 #endif
