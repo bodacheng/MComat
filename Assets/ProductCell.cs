9d1cf248988b40770a521ce700618cc27f25f479
伯大成 <bodacheng123@gmail.com>
2022-06-02 19:57:31 +0900

    尝试做个内购

---
+++ Assets/ProductCell.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ProductCell : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI msg;
+    [SerializeField] private TextMeshProUGUI price;
+}
