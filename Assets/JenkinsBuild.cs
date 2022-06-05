e03b4411f6cc221ff4f2f215f6ae7a77067d1ca9
伯大成 <bodacheng123@gmail.com>
2022-06-05 22:21:11 +0900

    增加了一个build addressbales的函数

--- Assets/JenkinsBuild.cs
+++ Assets/JenkinsBuild.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.Build.Reporting;
@@ -6,6 +7,10 @@ using UnityEngine;
 
 using System.IO;
 using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+
 public static class JenkinsBuild
 {
     static void KeystoreSet() {
@@ -42,6 +47,8 @@ public static class JenkinsBuild
         EditorUserBuildSettings.buildAppBundle = true;
         EditorUserBuildSettings.development = false;
 
+        AddressableAssetSettings.CleanPlayerContent(AddressableAssetSettingsDefaultObject.Settings.ActivePlayerDataBuilder);
+        
         KeystoreSet();
 
         BuildOptions options = BuildOptions.CompressWithLz4;
@@ -57,7 +64,76 @@ public static class JenkinsBuild
             Debug.Log("失败？：" +result.summary.result);
         }
     }
+    
+    public static string build_script 
+            = "Assets/AddressableAssetsData/DataBuilders/BuildScriptPackedMode.asset";
+        public static string settings_asset 
+            = "Assets/AddressableAssetsData/AddressableAssetSettings.asset";
+        public static string profile_name = "Default";
+        private static AddressableAssetSettings settings;
 
+        static void getSettingsObject(string settingsAsset) {
+            // This step is optional, you can also use the default settings:
+            //settings = AddressableAssetSettingsDefaultObject.Settings;
+
+            settings
+                = AssetDatabase.LoadAssetAtPath<ScriptableObject>(settingsAsset)
+                    as AddressableAssetSettings;
+
+            if (settings == null)
+                Debug.LogError($"{settingsAsset} couldn't be found or isn't " +
+                               $"a settings object.");
+        }
+
+        static void setProfile(string profile) {
+            string profileId = settings.profileSettings.GetProfileId(profile);
+            if (String.IsNullOrEmpty(profileId))
+                Debug.LogWarning($"Couldn't find a profile named, {profile}, " +
+                                 $"using current profile instead.");
+            else
+                settings.activeProfileId = profileId;
+        }
+
+        static void setBuilder(IDataBuilder builder) {
+            int index = settings.DataBuilders.IndexOf((ScriptableObject)builder);
+
+            if (index > 0)
+                settings.ActivePlayerDataBuilderIndex = index;
+            else
+                Debug.LogWarning($"{builder} must be added to the " +
+                                 $"DataBuilders list before it can be made " +
+                                 $"active. Using last run builder instead.");
+        }
+
+        static bool buildAddressableContent() {
+            AddressableAssetSettings
+                .BuildPlayerContent(out AddressablesPlayerBuildResult result);
+            bool success = string.IsNullOrEmpty(result.Error);
+
+            if (!success) {
+                Debug.LogError("Addressables build error encountered: " + result.Error);
+            }
+            return success;
+        }
+    
+    
+    [MenuItem("Window/Asset Management/Addressables/Build Addressables only")]
+    public static bool BuildAddressables() {
+        getSettingsObject(settings_asset);
+        setProfile(profile_name);
+        IDataBuilder builderScript
+            = AssetDatabase.LoadAssetAtPath<ScriptableObject>(build_script) as IDataBuilder;
+
+        if (builderScript == null) {
+            Debug.LogError(build_script + " couldn't be found or isn't a build script.");
+            return false;
+        }
+
+        setBuilder(builderScript);
+
+        return buildAddressableContent();
+    }
+    
     public static void BuildIOS()
     {
         Debug.Log ("[ScriptLog] Start Build iOS");
