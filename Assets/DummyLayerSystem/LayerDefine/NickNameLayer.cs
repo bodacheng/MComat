2f0cb362bbb5123cf34ad8bdd068279d44461b83
伯大成 <bodacheng123@gmail.com>
2022-11-02 16:54:53 +0900

    添加nickname脏词过滤。但目前用的这个插件在过滤中文上存在一些问题，英文的话貌似要好的多。考虑禁止用汉字

--- Assets/DummyLayerSystem/LayerDefine/NickNameLayer.cs
+++ Assets/DummyLayerSystem/LayerDefine/NickNameLayer.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Text.RegularExpressions;
 using DummyLayerSystem;
 using UnityEngine;
-using TMPro;
+using Crosstales.BWF;
+using UnityEngine.UI;
 
 public class NickNameLayer : UILayer
 {
-    [SerializeField] private TMP_InputField nickNameInput;
+    [SerializeField] private InputField nickNameInput;
     [SerializeField] private P3Button OK;
     [SerializeField] public P3Button Cancel;
     
@@ -16,6 +18,15 @@ public class NickNameLayer : UILayer
             setNickName.Invoke(nickNameInput.text);
         });
         Cancel.AddListener(UILayerLoader.Remove<NickNameLayer>);
+        nickNameInput.onEndEdit.AddListener(BadWordFilter);
+    }
+
+    void BadWordFilter(string currentTxt)
+    {
+        var outPutTxt= Regex.Replace(currentTxt, "[\\s\\p{P}\n\r=<>$>+￥^]", "");
+        outPutTxt = BWFManager.Instance.ReplaceAll(outPutTxt);
+        OK.interactable = !outPutTxt.Contains("*");
+        nickNameInput.text = outPutTxt;
     }
 
     public void LoadingRender(bool loading)
