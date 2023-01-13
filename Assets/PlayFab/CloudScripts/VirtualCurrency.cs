97fb15581dacec0f8ffac75e74029d08522a3389
伯大成 <bodacheng123@gmail.com>
2023-01-13 20:52:00 +0900

    VC substract

---
+++ Assets/PlayFab/CloudScripts/VirtualCurrency.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections.Generic;
+using System;
+
+public partial class CloudScript
+{
+    public static void SubtractVirtualCurrency(string code, int count, Action success)
+    {
+        if (Application.isPlaying)
+            ProgressLayer.Loading(string.Empty);
+        
+        PlayFabClientAPI.ExecuteCloudScript(
+            new ExecuteCloudScriptRequest
+            {
+                FunctionName = "SubtractVirtualCurrency",
+                FunctionParameter = new
+                {
+                    VirtualCurrencyCode = code,
+                    Count = count
+                },
+                GeneratePlayStreamEvent = true
+            },
+            (x)=>
+            {
+                ProgressLayer.Close();
+                Debug.Log(x.FunctionResult);
+                var returnValue = (PlayFab.Json.JsonObject) x.FunctionResult;
+                returnValue.TryGetValue("result", out var result);
+                var resultJson = (PlayFab.Json.JsonObject) result;
+                resultJson.TryGetValue("Balance", out var Balance);
+                int.TryParse(Balance.ToString(), out int intBalance);
+                resultJson.TryGetValue("VirtualCurrency", out var VirtualCurrency);
+                switch (VirtualCurrency.ToString())
+                {
+                    case "TK":
+                        Currencies.ArenaTicket.Value = intBalance;
+                        break;
+                    case "DM":
+                        Currencies.DiamondCount.Value = intBalance;
+                        break;
+                    case "GD":
+                        Currencies.CoinCount.Value = intBalance;
+                        break;
+                }
+                success.Invoke();
+            },
+            (x)=>
+            {
+                Debug.Log(x.Error);
+                ProgressLayer.Close();
+                PopupLayer.ArrangeWarnWindow(x.ErrorMessage);
+            }
+        );
+    }
+}
