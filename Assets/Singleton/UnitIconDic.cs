56cac9ea2bbbb24d1c068d5bfd509b56ff237917
伯大成 <bodacheng123@gmail.com>
2023-01-25 21:47:43 +0900

    settinglayer 还没整完

--- Assets/Singleton/UnitIconDic.cs
+++ Assets/Singleton/UnitIconDic.cs
@@ -1,25 +1,13 @@
-﻿using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Singleton
 {
     public static class UnitIconDic {
-    
-        static readonly IDictionary<string, Sprite> Dic = new Dictionary<string, Sprite>();
-    
+        
         public static async UniTask<Sprite> Load(string recordId, GameObject memoryReleaseTarget = null)
         {
-            Dic.TryGetValue(recordId, out Sprite sprite);
-            if (sprite == null)
-            {
-                sprite = await AddressablesLogic.LoadT<Sprite>("unit/" + recordId, memoryReleaseTarget);
-            }
-            else
-            {
-                return sprite;
-            }
-            DicAdd<string, Sprite>.Add(Dic, recordId, sprite);            
+            var sprite = await AddressablesLogic.LoadT<Sprite>("unit/" + recordId, memoryReleaseTarget);
             return sprite;
         }
     }
