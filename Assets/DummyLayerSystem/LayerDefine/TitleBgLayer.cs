823619d3f84073b35a1926d2d79b71ba08c94cb1
伯大成 <bodacheng123@gmail.com>
2023-01-26 23:22:04 +0900

    一个十分简单才对的scroll 页面困扰于谜一般的开头卡顿。

--- Assets/DummyLayerSystem/LayerDefine/TitleBgLayer.cs
+++ Assets/DummyLayerSystem/LayerDefine/TitleBgLayer.cs
@@ -1,19 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UniRx;
 
 public class TitleBgLayer : UILayer
 {
     [SerializeField] Image targetImage;
     [SerializeField] Sprite targetSprite;
-    [SerializeField] ScrollRect bgScrollRect;
     [SerializeField] RectTransform content;
     [SerializeField] Scrollbar vScrollbar;
+    [SerializeField] float duration = 10;
+    
+    private IDisposable _disposable;
     public void Setup()
     {
         content.sizeDelta = new Vector2(content.rect.width ,  targetSprite.rect.height * content.rect.width / targetSprite.rect.width);
         targetImage.sprite = targetSprite;
+        vScrollbar.value = 1;
+        targetImage.color = new Color(1,1,1,1);
+        var tweener = DOTween.To(() => vScrollbar.value, x => vScrollbar.value = x, 0, duration);
+        OnDestroyCallback.AddOnDestroyCallback(this.gameObject, ()=>{tweener.Kill();});
+        // _disposable = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1)).Subscribe(
+        //     (_) =>
+        //     {
+        //         vScrollbar.value = Mathf.Clamp(vScrollbar.value - 0.01f, 0, 1);
+        //         if (vScrollbar.value <= 0)
+        //             _disposable.Dispose();
+        //     }).AddTo(gameObject);
     }
 }
