8b36b90be23394c3292938107300f56b5c210d2e
伯大成 <bodacheng123@gmail.com>
2023-01-26 23:54:02 +0900

    继续思考为什么开始那个bglayer会往下滑一下。。

--- Assets/DummyLayerSystem/LayerDefine/TitleBgLayer.cs
+++ Assets/DummyLayerSystem/LayerDefine/TitleBgLayer.cs
@@ -9,10 +9,12 @@ using UniRx;
 public class TitleBgLayer : UILayer
 {
     [SerializeField] Image targetImage;
+    [SerializeField] Text subtitle;
     [SerializeField] Sprite targetSprite;
     [SerializeField] RectTransform content;
     [SerializeField] Scrollbar vScrollbar;
     [SerializeField] float duration = 10;
+    [SerializeField] List<string> subtitleCodes;
     
     private IDisposable _disposable;
     public void Setup()
@@ -21,14 +23,19 @@ public class TitleBgLayer : UILayer
         targetImage.sprite = targetSprite;
         vScrollbar.value = 1;
         targetImage.color = new Color(1,1,1,1);
-        var tweener = DOTween.To(() => vScrollbar.value, x => vScrollbar.value = x, 0, duration);
-        OnDestroyCallback.AddOnDestroyCallback(this.gameObject, ()=>{tweener.Kill();});
-        // _disposable = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1)).Subscribe(
-        //     (_) =>
-        //     {
-        //         vScrollbar.value = Mathf.Clamp(vScrollbar.value - 0.01f, 0, 1);
-        //         if (vScrollbar.value <= 0)
-        //             _disposable.Dispose();
-        //     }).AddTo(gameObject);
+        //var tweener = DOTween.To(() => vScrollbar.value, x => vScrollbar.value = x, 0, duration);
+        //OnDestroyCallback.AddOnDestroyCallback(this.gameObject, ()=>{tweener.Kill();});
+        _disposable = Observable.Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(0.1)).Subscribe(
+            (_) =>
+            {
+                vScrollbar.value = Mathf.Clamp(vScrollbar.value - 0.01f, 0, 1);
+                
+                int indexOfSubtitleCodes = (int)((1 - vScrollbar.value)/(vScrollbar.value/subtitleCodes.Count));
+                if (subtitleCodes.Count > indexOfSubtitleCodes)
+                    subtitle.text = subtitleCodes[indexOfSubtitleCodes];
+                
+                if (vScrollbar.value <= 0)
+                    _disposable.Dispose();
+            }).AddTo(gameObject);
     }
 }
