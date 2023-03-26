6386186648f6923548473a8480f58966fe4cbff2
伯大成 <bodacheng123@gmail.com>
2023-03-26 20:10:17 +0900

    安卓平台贴图格式转换

--- Assets/OtherTool/OtherTool.cs
+++ Assets/OtherTool/OtherTool.cs
@@ -55,68 +55,69 @@ public partial class OtherTool : EditorWindow
             SkillConfigFileUpdate(SkillConfigPath, SkillConfigFile, unitTypes);
         }
         
-        // if (GUILayout.Button("全项目所有贴图转换iphone格式"))
-        // {
-        //     Debug.Log("危险");
-        //     var guids = AssetDatabase.FindAssets("t:texture2D",  null);
-        //     foreach (var guid in guids)
-        //     {
-        //         string path = AssetDatabase.GUIDToAssetPath(guid);
-        //         TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
-        //         if (textureImporter != null)
-        //         {
-        //             TextureImporterPlatformSettings iPhone_png = new TextureImporterPlatformSettings
-        //             {
-        //                 overridden = true,
-        //                 name = "iPhone",
-        //                 maxTextureSize = 2048,
-        //                 format = TextureImporterFormat.ASTC_4x4,
-        //                 compressionQuality = 50,
-        //                 allowsAlphaSplitting = false
-        //             };
-        //
-        //             TextureImporterPlatformSettings iPhone_jpeg = new TextureImporterPlatformSettings
-        //             {
-        //                 overridden = true,
-        //                 name = "iPhone",
-        //                 maxTextureSize = 2048,
-        //                 format = TextureImporterFormat.ASTC_4x4,
-        //                 compressionQuality = 50,
-        //                 allowsAlphaSplitting = false
-        //             };
-        //
-        //             TextureImporterPlatformSettings Android_png = new TextureImporterPlatformSettings
-        //             {
-        //                 overridden = true,
-        //                 name = "Android",
-        //                 maxTextureSize = 2048,
-        //                 format = TextureImporterFormat.DXT5,
-        //                 compressionQuality = 50,
-        //                 allowsAlphaSplitting = false
-        //             };
-        //
-        //             TextureImporterPlatformSettings Android_jpeg = new TextureImporterPlatformSettings
-        //             {
-        //                 overridden = true,
-        //                 name = "Android",
-        //                 maxTextureSize = 2048,
-        //                 format = TextureImporterFormat.DXT1,
-        //                 compressionQuality = 50,
-        //                 allowsAlphaSplitting = false
-        //             };
-        //
-        //             if (textureImporter.DoesSourceTextureHaveAlpha ()) {
-        //                 //Alphaチャンネルあある場合
-        //                 textureImporter.SetPlatformTextureSettings (iPhone_png);
-        //                 textureImporter.SetPlatformTextureSettings (Android_png);
-        //             } else {
-        //                 //Alphaチャンネルがない場合
-        //                 textureImporter.SetPlatformTextureSettings (iPhone_jpeg);
-        //                 textureImporter.SetPlatformTextureSettings (Android_jpeg);
-        //             }
-        //         }
-        //     }
-        // }
+        if (GUILayout.Button("全项目所有贴图转换iphone格式"))
+        {
+            Debug.Log("危险");
+            var guids = AssetDatabase.FindAssets("t:texture2D",  null);
+            foreach (var guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+                if (textureImporter != null)
+                {
+                    Debug.Log("尝试更改以下贴图设置："+ path);
+                    TextureImporterPlatformSettings iPhone_png = new TextureImporterPlatformSettings
+                    {
+                        overridden = true,
+                        name = "iPhone",
+                        maxTextureSize = 2048,
+                        format = TextureImporterFormat.ASTC_4x4,
+                        compressionQuality = 50,
+                        allowsAlphaSplitting = false
+                    };
+        
+                    TextureImporterPlatformSettings iPhone_jpeg = new TextureImporterPlatformSettings
+                    {
+                        overridden = true,
+                        name = "iPhone",
+                        maxTextureSize = 2048,
+                        format = TextureImporterFormat.ASTC_4x4,
+                        compressionQuality = 50,
+                        allowsAlphaSplitting = false
+                    };
+        
+                    TextureImporterPlatformSettings Android_png = new TextureImporterPlatformSettings
+                    {
+                        overridden = false,
+                        name = "Android",
+                        maxTextureSize = 2048,
+                        //format = TextureImporterFormat.DXT5,
+                        compressionQuality = 50,
+                        allowsAlphaSplitting = false
+                    };
+        
+                    TextureImporterPlatformSettings Android_jpeg = new TextureImporterPlatformSettings
+                    {
+                        overridden = false,
+                        name = "Android",
+                        maxTextureSize = 2048,
+                        //format = TextureImporterFormat.ETC2_RGBA8,
+                        compressionQuality = 50,
+                        allowsAlphaSplitting = false
+                    };
+        
+                    if (textureImporter.DoesSourceTextureHaveAlpha ()) {
+                        //Alphaチャンネルあある場合
+                        //textureImporter.SetPlatformTextureSettings (iPhone_png);
+                        textureImporter.SetPlatformTextureSettings (Android_png);
+                    } else {
+                        //Alphaチャンネルがない場合
+                        //textureImporter.SetPlatformTextureSettings (iPhone_jpeg);
+                        textureImporter.SetPlatformTextureSettings (Android_jpeg);
+                    }
+                }
+            }
+        }
 
         #endregion
     }
