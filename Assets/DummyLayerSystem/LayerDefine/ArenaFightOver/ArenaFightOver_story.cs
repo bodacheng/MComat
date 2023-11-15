5e569b3b04e0e1e3b7f8850a4a01d482ac1e29eb
伯大成 <bodacheng123@gmail.com>
2023-11-15 14:45:47 +0900

    temp

---
+++ Assets/DummyLayerSystem/LayerDefine/ArenaFightOver/ArenaFightOver_story.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public partial class ArenaFightOver : UILayer
+{
+    [SerializeField] private Text shortStory;
+
+    public bool LoadStory()
+    {
+        var code = FightLoad.Fight.ID;
+        shortStory.text = ShortStory.Get(code);
+        return !string.IsNullOrEmpty(shortStory.text);
+    }
+}
