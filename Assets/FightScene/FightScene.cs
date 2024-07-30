74975bcf2f7ef15c1969bdaf9eedb39280b61d21
daisei <dacheng_bo@rudel.jp>
2024-07-30 22:16:01 +0900

    temp

--- Assets/FightScene/FightScene.cs
+++ Assets/FightScene/FightScene.cs
@@ -10,7 +10,7 @@ namespace FightScene
 {
     public class FightScene : MonoBehaviour
     {
-        [SerializeField] RectTransform T;
+        [SerializeField] Canvas canvas;
         [SerializeField] AudioSource audioSource;
         [SerializeField] AudioSource uiAudioSource;
         
@@ -69,12 +69,14 @@ namespace FightScene
         void Awake()
         {
             target = this;
+            PosCal.Canvas = this.canvas;
+            PosCal.TestIni();
         }
         
         void Start()
         {
             UILayerLoader.Clear();
-            UILayerLoader.SetHanger(T);
+            UILayerLoader.SetHanger(canvas.GetComponent<RectTransform>());
             
             //HighLightLayer.DarkOff(Color.white, 0, true);
             Time.timeScale = 1;
