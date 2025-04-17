321c0edce5f98066ec88a5761d8ba635c1eaf164
daisei <dacheng_bo@rudel.jp>
2025-04-17 17:24:39 +0900

    修正bullet claw

--- Assets/Behaviour/Defines/AI_States_Blueprints/Behavior.cs
+++ Assets/Behaviour/Defines/AI_States_Blueprints/Behavior.cs
@@ -273,7 +273,7 @@ namespace Soul
                 var touchingECenter = _BasicPhysicSupport.hiddenMethods.GetCenterOfTouchingEnemies();
                 if (touchingECenter != Vector3.zero)
                 {
-                    _Rigidbody.AddForce((_DATA_CENTER.WholeT.position - touchingECenter), ForceMode.Acceleration);
+                    _Rigidbody.AddForce(_DATA_CENTER.WholeT.position - touchingECenter, ForceMode.VelocityChange);
                 }
             }
         }
