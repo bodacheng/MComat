5e2735f5e9a899ed5b99b7d3150f83e8e47a6658
daisei <dacheng_bo@rudel.jp>
2025-05-07 23:06:34 +0900

    改善战斗开始按钮

--- Assets/Resources/DummyLayerSystem/FightPrepareLayer/FightBtn/FightBeginBtn.cs
+++ Assets/Resources/DummyLayerSystem/FightPrepareLayer/FightBtn/FightBeginBtn.cs
@@ -1,10 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine.UI;
 
 public class FightBeginBtn : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private BOButton btn;
+    [SerializeField] List<Image> texts;
+    [SerializeField] private float duration;
+    
+    private float titleAnimFactor = 0;
+    private TweenerCore<float, float, FloatOptions> titleTween;
+
+    private void OnEnable()
+    {
+        titleAnimFactor = 0;
+        SetTexts(titleAnimFactor);
+    }
 
     public void SetAction(Action action)
     {
@@ -15,7 +32,39 @@ public class FightBeginBtn : MonoBehaviour
     public void Enable(bool on, bool guide = false)
     {
         btn.interactable = on;
-        animator.SetBool("On", on);
+        //animator.SetBool("On", on);
+
+        float endValue;
+        if (on)
+        {
+            endValue = 1;
+        }
+        else
+        {
+            endValue = 0;
+        }
+        
+        titleTween?.Kill();
+        titleTween = DOTween.To(() => titleAnimFactor, (x) => titleAnimFactor = x, endValue, duration)
+            .OnUpdate(
+                () =>
+                {
+                    SetTexts(titleAnimFactor);
+                }
+            );
+    }
+
+    void SetTexts(float value)
+    {
+        texts.ToList().ForEach(x =>
+        {
+            x.material.SetFloat("_Animation_Factor", value);
+        });
+    }
+    
+    private void OnDestroy()
+    {
+        titleTween?.Kill();
     }
 }
 
