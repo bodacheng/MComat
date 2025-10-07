a0354e3704144342d9ed0f0e7b00589b83599c9f
daisei <dacheng_bo@rudel.jp>
2025-10-07 16:56:42 +0900

    fix

--- Assets/TheNineSlot/MiniNineSlots/NineForShow_Util.cs
+++ Assets/TheNineSlot/MiniNineSlots/NineForShow_Util.cs
@@ -87,7 +87,7 @@ public partial class NineForShow : MonoBehaviour
     {
         await UniTask.WhenAll(
             SkillSetStateRender(
-                PreScene.target.postProcessCamera,
+                PreScene.target.noPostProcessCamera,
                 set.a1, set.a2, set.a3,
                 set.b1, set.b2, set.b3,
                 set.c1, set.c2, set.c3, 
